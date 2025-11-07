2e3f5878ea9e30587534eb55ca90965b0a0ba5e5
Patrick Hennessey <7787945+pahenn@users.noreply.github.com>
2025-11-07 09:56:20 -0600

    add ghostty

--- flake.nix
+++ flake.nix
@@ -39,7 +39,9 @@
         # Create /etc/zshrc that loads the nix-darwin environment.
         # programs.zsh.enable = true;
 
-        environment.systemPackages = [ ];
+        environment.systemPackages = [
+          pkgs.ghostty
+        ];
 
 
         # The platform the configuration will be used on.
